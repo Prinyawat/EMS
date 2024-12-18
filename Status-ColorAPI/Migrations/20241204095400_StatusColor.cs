dab51b1ac67cc69964323271a5b62b174a47b2a5
doprinyw <50543prinyawat@gmail.com>
2024-12-18 17:31:00 +0700

    add file

---
+++ Status-ColorAPI/Migrations/20241204095400_StatusColor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Status_ColorAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class StatusColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StatusColors",
+                columns: table => new
+                {
+                    StatusId = table.Column<long>(type: "bigserial", nullable: false),
+                    StatusType = table.Column<string>(type: "varchar(50)", nullable: true),
+                    StatusCode = table.Column<string>(type: "varchar(50)", nullable: true),
+                    StatusName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Active = table.Column<bool>(type: "bool", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusColors", x => x.StatusId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StatusColors");
+        }
+    }
+}
---
+++ Status-ColorAPI/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Status_ColorAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StatusColorContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(options =>
+options.WithOrigins("http://localhost:4200")
+.AllowAnyMethod()
+.AllowAnyHeader());
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
