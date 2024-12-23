7bc8936a997eca0ee7d60af736564c1d569e18a9
pullmecloser <worldwidehandsome0507@gmail.com>
2024-12-23 16:21:09 +0700

    [Feat] add botton cancel, add toster cancel notification and add modal อ่านเพิ่มเติม

--- Ems.App/ClientApp/src/app/pages/course/registration-history-course/registration-history-course.component.ts
+++ Ems.App/ClientApp/src/app/pages/course/registration-history-course/registration-history-course.component.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-registration-history-course',
   templateUrl: './registration-history-course.component.html',
+  providers: [MessageService]
 })
 export class RegistrationHistoryCourseComponent {
+  
+  display: boolean = false;
+  
+    constructor(
+      private messageService: MessageService
+    ){}
+    
+  courses = [
+    {
+      id: 1,
+      title: 'Angular',
+      subtitle: 'คอร์สเรียนรู้พื้นฐาน การใช้ Angular',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      openDate: new Date(2024, 0, 15), 
+      closeDate: new Date(2024, 0, 20), 
+      openTime: '09:00', 
+      closeTime: '17:00', 
+      display: false, 
+    },
+    {
+      id: 2,
+      title: 'React',
+      subtitle: 'คอร์สเรียนรู้พื้นฐาน การใช้ React',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      openDate: new Date(2024, 0, 25), 
+      closeDate: new Date(2024, 0, 30), 
+      openTime: '09:00', 
+      closeTime: '17:00', 
+      display: false,
+    },
+  ]
+
+  showCanCelViaToast(courseId: number) {
+    console.log('ยกเลิกการลงทะเบียน:', courseId);
+    this.messageService.add({ key: 'tst', severity: 'error', summary: 'ยกเลิกการลงทะเบียน', detail: 'คุณได้ยกเลิกการลงทะเบียนแล้ว' });
+}
 
 }
--- Ems.App/ClientApp/src/app/pages/course/registration-history-course/registration-history-course.module.ts
+++ Ems.App/ClientApp/src/app/pages/course/registration-history-course/registration-history-course.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RegistrationHistoryCourseRoutingModule } from './registration-history-course-routing.module';
+import { RegistrationHistoryCourseComponent } from './registration-history-course.component';
 
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
-    RegistrationHistoryCourseRoutingModule
-  ]
+    RegistrationHistoryCourseRoutingModule,
+    DialogModule,
+    ButtonModule,
+    ToastModule
+  ],
+  declarations: [RegistrationHistoryCourseComponent]
 })
 export class RegistrationHistoryCourseModule { }
