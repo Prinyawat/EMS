fc74c26ba599623dc5c6c00c0066ae2f004b1a0a
mumei1112 <157720569+mumei1112@users.noreply.github.com>
2024-12-23 13:00:56 +0700

    [Feat, Fix]Install Leaflet + Edit Checking

--- Ems.App/ClientApp/src/app/pages/checking/checking.component.ts
+++ Ems.App/ClientApp/src/app/pages/checking/checking.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 @Component({
     selector: 'app-checking',
-    templateUrl: './checking.component.html'
+    templateUrl: './checking.component.html',
+    providers: []
 })
 export class CheckingComponent {
+    valRadio: string = '';
+
+    display: boolean = false;
+
+    private map!: L.Map;
+
+    ngOnInit(): void {
+        // พิกัดเริ่มต้นของแผนที่
+        this.map = L.map('map').setView([18.7953, 98.9989], 13); // พิกัดของเชียงใหม่ (latitude, longitude)
+
+        // ใช้ OpenStreetMap สำหรับแผนที่
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+      }
 
 }
--- Ems.App/ClientApp/src/app/pages/checking/checking.module.ts
+++ Ems.App/ClientApp/src/app/pages/checking/checking.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CheckingRoutingModule } from './checking-routing.module';
+import { CalendarModule } from 'primeng/calendar';
+import { CheckingComponent } from './checking.component';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { InputDemoRoutingModule } from 'src/app/demo/components/uikit/input/inputdemo-routing.module';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { DialogModule } from 'primeng/dialog';
+
 
 
 @NgModule({
     imports: [
-        CheckingRoutingModule
+        CheckingRoutingModule,
+        CommonModule,
+        FormsModule,
+        InputDemoRoutingModule,
+        CalendarModule,
+        InputTextModule,
+        InputTextareaModule,
+        RadioButtonModule,
+        DialogModule,
     ],
+        declarations: [CheckingComponent]
 })
 export class CheckingComponentModule { }
