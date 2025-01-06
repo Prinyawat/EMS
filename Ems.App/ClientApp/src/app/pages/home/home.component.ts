370ce0b9dd59f5ddbd58f8435ef0ae567fb00dec
pullmecloser <worldwidehandsome0507@gmail.com>
2025-01-06 14:25:06 +0700

    [Fix] export issue in HomeModule and HomeRoutingModule

--- Ems.App/ClientApp/src/app/pages/home/home.component.ts
+++ Ems.App/ClientApp/src/app/pages/home/home.component.ts
@@ -4,6 +4,6 @@ import { Component } from "@angular/core";
     selector: 'app-home',
     templateUrl: './home.component.html'
 })
-export class LeaveRequestComponent {
+export class HomeComponent {
 
 }
