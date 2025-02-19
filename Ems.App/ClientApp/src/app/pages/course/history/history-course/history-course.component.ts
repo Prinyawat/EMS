58e583b200cebc52795190be05107c5c993a8e40
pullmecloser <worldwidehandsome0507@gmail.com>
2025-02-19 17:53:18 +0700

    [Fix] certificate

--- Ems.App/ClientApp/src/app/pages/course/history/history-course/history-course.component.ts
+++ Ems.App/ClientApp/src/app/pages/course/history/history-course/history-course.component.ts
@@ -61,38 +61,94 @@ export class HistoryCourseComponent {
     const doc = new jsPDF({
       orientation: "landscape",
       unit: "mm",
-      format: "A4"
+      format: "A4",
     });
   
     const pageWidth = doc.internal.pageSize.getWidth();
+    const pageHeight = doc.internal.pageSize.getHeight();
   
     doc.setFontSize(24);
     doc.setFont("helvetica", "bold");
-    doc.text("SOFTSQUARE GROUP", pageWidth / 2, 50, { align: "center" });
+    doc.text("SOFTSQUARE GROUP", pageWidth / 2, 30, { align: "center" });
   
-    doc.setFontSize(18);
-    doc.setFont("helvetica", "normal");
-    doc.text("This certificate is proudly presented to", pageWidth / 2, 70, { align: "center" });
+    doc.setLineWidth(0.5);
+    doc.line(20, 35, pageWidth - 20, 35); 
   
     doc.setFontSize(22);
-    doc.setFont("times", "italic", "bold");
-    doc.text(`${this.user.firstname} ${this.user.lastname}`, pageWidth / 2, 90, { align: "center" });
+    doc.setFont("times", "bold");
+    doc.text("CERTIFICATE OF ACHIEVEMENT", pageWidth / 2, 50, { align: "center" });
   
     doc.setFontSize(16);
     doc.setFont("helvetica", "normal");
-    doc.text("For successfully completing the course", pageWidth / 2, 110, { align: "center" });
-    
+    doc.text("This certificate is proudly presented to", pageWidth / 2, 70, {
+      align: "center",
+    });
+  
+    doc.setFontSize(24);
+    doc.setFont("times", "italic", "bold");
+    doc.text(`${this.user.firstname} ${this.user.lastname}`, pageWidth / 2, 90, {
+      align: "center",
+    });
+  
+    doc.setFontSize(16);
+    doc.setFont("helvetica", "normal");
+    doc.text("For successfully completing the course", pageWidth / 2, 110, {
+      align: "center",
+    });
+  
     doc.setFontSize(18);
     doc.setFont("times", "bold");
     doc.text(course.courseName, pageWidth / 2, 130, { align: "center" });
-    
+  
+    doc.setFontSize(14);
+    doc.setFont("helvetica", "italic");
+    doc.text(
+      "In recognition of the dedication and effort demonstrated during the training.",
+      pageWidth / 2,
+      145,
+      { align: "center" }
+    );
+  
     doc.setFontSize(16);
     doc.setFont("helvetica", "normal");
-    doc.text("In recognition of the dedication and effort demonstrated during the training.", pageWidth / 2, 150, { align: "center" });
-    
-    doc.setFontSize(14);
+    doc.text(
+      `This course was created and managed by the Administrator on behalf of the company.`,
+      pageWidth / 2,
+      160,
+      { align: "center" }
+    );
+  
+    const signatureImage = "assets/image/signature.png";
+    const signatureWidth = 50; 
+    const signatureHeight = 20; 
+    doc.addImage(
+      signatureImage,
+      "PNG",
+      pageWidth / 2 - signatureWidth / 2,
+      170,
+      signatureWidth,
+      signatureHeight
+    );
+  
+    doc.setFontSize(12);
+    doc.setFont("helvetica", "normal");
+    const date = new Date();
+    const formattedDate = `${String(date.getDate()).padStart(2, "0")}/${String(
+      date.getMonth() + 1
+    ).padStart(2, "0")}/${date.getFullYear()}`;
+    doc.text(`Date: ${formattedDate}`, pageWidth - 40, pageHeight - 20, {
+      align: "right",
+    });
+  
+    doc.setLineWidth(0.5);
+    doc.line(20, pageHeight - 25, pageWidth - 20, pageHeight - 25);
+    doc.setFontSize(12);
+    doc.text("SOFTSQUARE GROUP", pageWidth / 2, pageHeight - 15, {
+      align: "center",
+    });
   
     doc.save(`${course.courseName}_certificate.pdf`);
   }
+  
   
 }
